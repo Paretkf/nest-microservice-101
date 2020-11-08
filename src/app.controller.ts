dbeb224e0f4aa55444601e583a748735bede7714
paretk7 <paretk7@gmail.com>
2020-11-08 23:17:44 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { AppService } from './app.service';
+import { MathService } from './math.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly mathService: MathService
+  ) {}
+
+  private logger = new Logger('AppController')
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('add')
+  @MessagePattern('add')
+  async accumulate(data: number[]): Promise<number> {
+    return await this.mathService.accumulate(data)
+  }
+}
